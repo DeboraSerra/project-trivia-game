0e92d52dff0b4e6866ad3e8706e73059406bc55f
Débora Serra <debora.r.serra@gmail.com>
2022-04-07 11:15:52 -0300

    Updates

---
+++ precache-manifest.111aed5945546ab96d5ca8df95d3dddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693ada23281e5e002255a759979c57e4",
+    "url": "/project-trivia-game/index.html"
+  },
+  {
+    "revision": "856cdd1da6bfae2be89b",
+    "url": "/project-trivia-game/static/css/main.86159d4f.chunk.css"
+  },
+  {
+    "revision": "c0c1b693ca130250f669",
+    "url": "/project-trivia-game/static/js/2.dc7a567a.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project-trivia-game/static/js/2.dc7a567a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856cdd1da6bfae2be89b",
+    "url": "/project-trivia-game/static/js/main.4c8d6ecc.chunk.js"
+  },
+  {
+    "revision": "1e534896b255546bdc25",
+    "url": "/project-trivia-game/static/js/runtime-main.b3448e5f.js"
+  }
+]);
